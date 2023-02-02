8ef08bab09e4bc357b809c1c17253d154c115c04
Jeremy Magland <jmagland@flatironinstitute.org>
2023-02-02 09:38:40 -0500

    transfer to flatironinstitute

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://magland.github.io/mcmc-monitor"
+  base: "https://flatironinstitute.github.io/mcmc-monitor"
 })
