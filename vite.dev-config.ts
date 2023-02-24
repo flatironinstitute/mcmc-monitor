83b23175fff7d3cd1f9838689ebc0a8ef6e6f2a0
Jeff Soules <jsoules@flatironinstitute.org>
2023-02-24 11:48:15 -0500

    Add separate build dir for dev deployment.

--- vite.dev-config.ts
+++ vite.dev-config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   base: "https://flatironinstitute.github.io/mcmc-monitor/dev",
+  build: {
+    outDir: "dev-dist"
+  },
   resolve: {
     alias: {
       "simple-peer": "simple-peer/simplepeer.min.js"
